f57878423fcd9f6fa8f66b1cc6e9c8e079faf797
Ayaz <muhammadayyazmunir@gmail.com>
2024-06-25 11:56:15 +0500

    first commit

---
+++ src/main/java/com/example/Java_terra/JavaTerraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_terra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTerraApplication.class, args);
+	}
+
+}
